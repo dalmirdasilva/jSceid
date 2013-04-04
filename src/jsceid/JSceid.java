55559a40ddb4ac7a2af25ead28dd554f070f0a8c
Dalmir <dalmir@debian>
2013-04-04 00:57:06 -0700

    sync

--- src/jsceid/JSceid.java
+++ src/jsceid/JSceid.java
@@ -12,7 +12,8 @@ import dao.hibernate.HibernateDAOFactory;
 import dao.hibernate.InstitutionHibernateDAO;
 import dao.hibernate.MedicHibernateDAO;
 import dao.hibernate.PatientHibernateDAO;
-import entity.Address;
+import entity.Medic;
+import entity.Patient;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.hibernate.SessionFactory;
@@ -41,16 +42,34 @@ public class JSceid {
         ClinicalDAO clinicalDAO = (ClinicalDAO) daoFactory.getNewDAO(ClinicalHibernateDAO.class);
         AddressDAO addressDAO = (AddressDAO) daoFactory.getNewDAO(AddressHibernateDAO.class);
         InstitutionDAO institutionDAO = (InstitutionDAO) daoFactory.getNewDAO(InstitutionHibernateDAO.class);
+
+        Medic m = medicDAO.findAll().get(0);
+        Patient p = new Patient(m, "Paulo2", 111, 100, true, "white", "agricultor", "asdasd", "", "", "", true);
+        //patientDAO.saveEntity(p);
+        //m.getPatients().add(p);
+        System.out.println(m.getPatients());
+        medicDAO.saveEntity(m);
         
-        addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Dos Andradas", 1000));
-        addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Almirantes", 123));
-        addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Andradas", 1002));
-        addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Henrique", 12005));
         
-        for (Address a : addressDAO.findAll()) {    
-            System.out.println(a.getNumber());
-        }
         
+        //addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Dos Andradas", 1000));
+        //addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Almirantes", 123));
+        //addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Andradas", 1002));
+        //addressDAO.saveEntity(new Address("Brasil", "RS", "POA", "Henrique", 12005));
+
+        //Address address = addressDAO.findById((long) 1, false);
+        //institutionDAO.saveEntity(new Institution(address, "UFSM", "ufsm@ufsm.com", "55999888"));
+        
+        //for (Institution i : institutionDAO.findAll()) {
+        //    System.out.println(i.getName());
+        //}
+
+        //Institution i = institutionDAO.findAll().get(0);
+        
+        //i.setAddress(new Address("US", "RS", "asd", "asdads", 1000));
+        
+        //institutionDAO.saveEntity(i);
+
         /*
          patientDAO.saveEntity(new Patient(0, "Pedroso", 122, 56, true, "Amarelo", "Estudante", "Rua da quadra", "LS", "LS", "5588774455", true));
          medicDAO.saveEntity(new Medic(0, "Pedro", "5599254645", "pedro@gmail.com", "POA", true));
@@ -60,14 +79,14 @@ public class JSceid {
          }
          */
         //List<Medic> ms = medicDAO.findByExample(new Medic(0, "Pedro", "5599254645", "pedro@gmail.com", "POA", true), null);
-        
+
         //Patient p = new Patient(ms.get(0), "Pedrosinho", 122, 56, true, "Amarelo", "Estudante", "Rua da quadra", "LS", "LS", "5588774455", true);
         //patientDAO.saveEntity(p);
-        
+
         //for (Patient p1 : patientDAO.findAll()) {    
         //    System.out.println(p1.getMedic().getCity());
         //}
-        
+
         //for (Patient p : ms.get(0).getPatients()) {
         //    System.out.println(p.getName());
         //}
